a41099d4f3161a82a8b6794f9055ac9bf84865e5
Jansen Smith <jansen@bstarengineering.com>
2024-07-24 14:46:13 -0400

    imported pick csg and improved sig csg

--- TreGuitarPick.groovy
+++ TreGuitarPick.groovy
@@ -1,7 +1,11 @@
 
 
 // import pick STL
-
+File pickSTL = ScriptingEngine.fileFromGit(
+	"https://github.com/JansenSmith/TreGuitarPick.git",
+	"pick.stl");
+// Load the .CSG from the disk and cache it in memory
+CSG pick  = Vitamins.get(pickSTL);
 
 // import sig SVG
 File f = ScriptingEngine
@@ -19,8 +23,11 @@ HashMap<String,List<Polygon>> polygonsByLayer = s.toPolygons()
 HashMap<String,ArrayList<CSG>> csgByLayers = s.extrudeLayers(10)
 // extrude just one layer to 10mm
 // The string "1-holes" represents the layer name in Inkscape
-def holeParts = s.extrudeLayerToCSG(10,"insides")
+def insideParts = s.extrudeLayerToCSG(10,"insides")
 // seperate holes and outsides using layers to differentiate
 // The string "2-outsides" represents the layer name in Inkscape
 def outsideParts = s.extrudeLayerToCSG(10,"outside")
-					.difference(holeParts)
+
+CSG sig = outsideParts.difference(insideParts)
+
+return sig
